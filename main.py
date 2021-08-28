d2ae0c6218d863226905e4b01e952519d95b3f1c
Роберт <pobept46.2002@gmail.com>
2021-08-29 01:29:58 +0500

    Update main.py

--- main.py
+++ main.py
@@ -53,7 +53,7 @@ def sometext(link,yaw,pcx,pcz,cx,cz,plr,world,players):
     urllib.request.urlretrieve(link,tfl)
     marker = Image.open("marker.png")
     marker=marker.rotate(yaw)
-    img = Image.open(tfl)
+    img = Image.open(bytes(tfl,'utf-8'))
     draw = ImageDraw.Draw(img)
     font = ImageFont.truetype("Minecraft.otf", 10)
     shadowcolor="black"
